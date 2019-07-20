cb0d34a4a4e39d51f20f9a687266f3d969cd2cf3
Acastillologsys <acastillo@logsys.com.mx>
2019-07-20 15:27:46 -0500

    .

--- platzigram/settings.py
+++ platzigram/settings.py
@@ -83,11 +83,14 @@ WSGI_APPLICATION = 'platzigram.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'platzi',
+        'USER': 'freddier',
+        'PASSWORD': 'cvander<3',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
